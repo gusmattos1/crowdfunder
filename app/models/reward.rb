bcd191da8802802dcdf59aaffc0b6c85c66306ea
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-05-01 19:09:29 -0400

    validations continue

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -2,5 +2,14 @@ class Reward < ActiveRecord::Base
   belongs_to :project
 
 validates :dollar_amount, :description, presence: true
+validate :reward_dollar_amount_must_be_a_positive_number
+
+
+  def reward_dollar_amount_must_be_a_positive_number
+    if dollar_amount && dollar_amount <= 0
+      errors.add(:dollar_amount, "Reward amount must be greater than $0")
+    end
+
+  end
 
 end
