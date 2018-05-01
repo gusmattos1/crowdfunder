cfe7842a4c5291f2ea75320d57563639b62afab3
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-05-01 16:09:28 -0400

    assignment pt 1 complete

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,4 +4,13 @@ class Pledge < ApplicationRecord
 
   validates :dollar_amount, presence: true
   validates :user, presence: true
+  validate  :user_should_not_be_the_onwer
+
+def user_should_not_be_the_onwer
+
+    if self.user == project.user
+
+      errors.add(:user, "Owner should not be able to pledge towards own project")
+    end
+  end
 end
