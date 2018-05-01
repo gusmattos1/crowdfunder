8fe4e170b7ef62de96e97993208834d3e4d72f07
gusmattos1 <gusmattos1@gmail.com>
2018-05-01 16:06:02 -0400

    assignment part 1 done
    cq

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,4 +4,15 @@ class Pledge < ApplicationRecord
 
   validates :dollar_amount, presence: true
   validates :user, presence: true
+
+  validate :user_should_not_be_the_owner
+
+  def user_should_not_be_the_owner
+
+    if self.user == project.user
+
+      errors.add(:user, "Owner should not be able to pledge towards own project")
+    end
+  end
+
 end
--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,3 +1,6 @@
 class Reward < ActiveRecord::Base
   belongs_to :project
+
+  validates :dollar_amount, :description,  presence: true
+
 end
