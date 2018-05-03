9b7db8733304bc7f5e40de8c7b37bdcd69ccdd7d
gusmattos1 <gusmattos1@gmail.com>
2018-05-03 15:40:15 -0400

    befopre merging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def ensure_logged_in
+    unless current_user
+      flash[:alert] = "Please log in"
+      redirect_to login_path
+    end
   end
 
   helper_method :current_user
 
   private
+
   def not_authenticated
     redirect_to login_path, alert: "Please login first"
   end
+
 end
